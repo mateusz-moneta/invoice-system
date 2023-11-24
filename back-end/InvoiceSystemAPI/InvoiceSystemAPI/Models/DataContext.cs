da8319f1430d65e35f0692b66d3df0ffa6ee4ac1
Kim Aloshyn <kim.aloshyn@noaignite.com>
2023-11-24 17:26:24 +0100

    initial backend commit

---
+++ back-end/InvoiceSystemAPI/InvoiceSystemAPI/Models/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+using System.Text.RegularExpressions;
+
+namespace InvoiceSystemAPI.Models
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ back-end/InvoiceSystemAPI/InvoiceSystemAPI/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace InvoiceSystemAPI.Models
+{
+    public class User : BaseEntity
+    {
+        public string Email { get; set; }
+
+        public string Login { get; set; }
+
+        public string Name { get; set; }
+
+        public string? Surname { get; set; }
+    }
+}

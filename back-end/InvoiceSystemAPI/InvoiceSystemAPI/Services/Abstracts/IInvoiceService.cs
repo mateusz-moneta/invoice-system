80da977dd39dba260337ec68832dec4134331e30
Andrzej Polak <andrzejp2805@gmail.com>
2024-01-10 15:13:57 +0100

    Add service, new model to InvoicesController

---
+++ back-end/InvoiceSystemAPI/InvoiceSystemAPI/Services/Abstracts/IInvoiceService.cs
@@ -0,0 +1,14 @@
+﻿using InvoiceSystemAPI.Models;
+using InvoiceSystemAPI.Requests;
+
+namespace InvoiceSystemAPI.Services.Abstracts
+{
+    public interface IInvoiceService
+    {
+        Task CreateInvoice(Invoice invoice);
+        Task<List<Invoice>> GetAllInvoicesAsync();
+        Task<Invoice> GetInvoiceByIdAsync(int id);
+        Task<bool> UpdateInvoiceAsync(Invoice updatedInvoice);
+        Task DeleteInvoiceAsync(int id);
+    }
+}

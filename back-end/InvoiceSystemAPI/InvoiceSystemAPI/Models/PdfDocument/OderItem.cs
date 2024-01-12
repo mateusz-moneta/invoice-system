104a3f9049a771cda132e14fb4de402ce86dc512
Kim Aloshyn <kim.aloshyn@noaignite.com>
2024-01-12 18:14:28 +0100

    add: pdf creation models layer

---
+++ back-end/InvoiceSystemAPI/InvoiceSystemAPI/Models/PdfDocument/OderItem.cs
@@ -0,0 +1,9 @@
+﻿namespace InvoiceSystemAPI.Models.PdfDocument
+{
+    public class OrderItem
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}

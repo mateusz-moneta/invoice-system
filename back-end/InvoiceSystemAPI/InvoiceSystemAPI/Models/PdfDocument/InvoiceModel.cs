0aff6ba8c566e1060ee6865145aaf4564e3b8523
Kim Aloshyn <kim.aloshyn@noaignite.com>
2024-01-12 18:14:28 +0100

    add: pdf creation models layer

---
+++ back-end/InvoiceSystemAPI/InvoiceSystemAPI/Models/PdfDocument/InvoiceModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+
+namespace InvoiceSystemAPI.Models.PdfDocument
+{
+    public class InvoiceModel
+    {
+        public int InvoiceNumber { get; set; }
+        public DateTime IssueDate { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public Address SellerAddress { get; set; }
+        public Address CustomerAddress { get; set; }
+
+        public List<OrderItem> Items { get; set; }
+        public string Comments { get; set; }
+    }
+}
